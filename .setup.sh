8930dae42de5b14d036646774f66bebe73b2ae08
Joshua Newton <joshuacwnewton@gmail.com>
2023-08-27 17:19:48 -0400

    .setup.sh: Add commands to set up new workstation

---
+++ .setup.sh
@@ -0,0 +1,34 @@
+# setup keyboard shortcut to open terminal with Super+T
+
+# install necessary programs
+sudo apt install git
+
+# setup ssh key first (temporary until backed-up ssh keys can be reloaded)
+# - https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
+# - https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account
+
+# setup dotfiles repository
+cd ~
+git init .
+git branch -m main
+git remote add origin git@github.com:joshuacwnewton/dotfiles.git
+git fetch
+# move/remove any listed conflicting files
+mv ~/.bashrc ~/.bashrc.bak
+mv ~/.profile ~/.profile.bak
+rm ~/.gitconfig
+rm ~/.bash_logout
+# checkout main branch
+git checkout main
+
+# install programs post-dotfile installation
+sudo apt install gnome-tweaks # Keyboard & Mouse -> Middle Click Paste off
+# openconnect -> needed for PolyMtl VPN
+sudo apt install openconnect network-manager-openconnect network-manager-openconnect-gnome
+
+# install pycharm
+cd ~/Downloads
+sudo tar xzf pycharm-*.tar.gz -C /opt/
+cd /opt/pycharm-*/bin
+sh pycharm.sh
+# change terminal settings to "/bin/bash --rcfile ~/.pycharmrc"
